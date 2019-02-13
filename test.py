71e6811688eb95db43408396e84094a9ab2304a4
Jacob Olander <31401560+mrjacola@users.noreply.github.com>
2019-02-13 13:32:40 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+# Modules
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+
+# Home made module
+import remove_diag as rd
 
 # Constants for book keeping
 
@@ -19,30 +24,54 @@ nr_steps = int(t_max/h)
 # Allocation
 x = np.zeros([N, d, nr_steps])
 v = np.zeros([N, d, nr_steps])
+dx = np.zeros([N, N, d])
 
 # Init
 x[:,:,0] = L*(np.random.rand(N, d) - 0.5)
 v[:,:,0] = v_range*(np.random.rand(N, d) - 0.5)
 
-r = np.zeros([N, N, d])
 
-for i in range(N):
-    for j in range(N):
+# Run simulation in time
+for t in range(nr_steps - 1):
 
-        r[i, j, :] = x[i, :, 0] - x[j, :, 0]
+    # Calc all coordinate wise distances between particles
+    for i in range(N):
+        for j in range(N):
 
-r[r > L/2] = r[r > L/2] - L
-r[r < -L/2] = r[r < -L/2] + L
+            dx[i, j, :] = x[i, :, t] - x[j, :, t]
 
-print(r[r[:]!=0])
-#F = np.sum(24*(1 - 2/np.power(r[r!=0], 6))/np.power(r[r!=0], 7), 0)
+    # Implement boundary nearest image convention
+    dx[dx > L/2] = dx[dx > L/2] - L
+    dx[dx < -L/2] = dx[dx < -L/2] + L
 
-#print(F)
+    # Remove zeros on diagonal
+    dx_no_diag = np.zeros([N, N - 1, d])
 
+    for i in range(d):
+        dx_no_diag[:,:,i] = rd.remove_diag(dx[:,:,i])
 
+    # Calc distance between
+    r = np.sqrt(np.sum(np.square(dx_no_diag), 2))
 
-#plt.scatter(x[:,0,0], x[:,1,0])
-#plt.show()
+    x_vec = dx_no_diag
 
-#print(type(x[0,0,0]))
-#print(v_0)
+    # Calc force vector on each particle
+    F = -(np.sum((24*(1 - 2*np.power(r, -6))*np.power(r, -8)).T*x_vec.T, 1)).T
+
+    # Update state variables
+    v[:,:,t+1] = v[:,:,t] + F*h
+    x[:,:,t+1] = x[:,:,t] + v[:,:,t]*h
+
+    # Implement boundary conditions
+    for dim in range(d):
+        x[:,dim,t + 1][x[:,dim,t + 1] > L/2] = x[:,dim,t + 1][x[:,dim,t + 1] > L/2] - L
+        x[:,dim,t + 1][x[:,dim,t + 1] < -L/2] = x[:,dim,t + 1][x[:,dim,t + 1] < -L/2] + L
+
+print(np.max(x))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+for t in range(nr_steps - 1):
+    ax.clear()
+    ax.scatter(x[:,0,t], x[:,1,t])
+    plt.pause(1)
