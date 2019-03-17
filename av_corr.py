747676aff0d609467a5e52465701b63d2e84f1a6
adonvel <47566922+adonvel@users.noreply.github.com>
2019-03-17 22:20:38 +0100

    Create av_corr.py

---
+++ av_corr.py
@@ -0,0 +1,12 @@
+#This function requires the of corr and its calculation for every time step, as well as bin_edges
+
+
+import numpy as np
+import math
+
+
+def g(corr,bin_edges,L,act_N):
+    delta_R=bin_edges[-1]/(len(bin_edges)-1)
+    R=bin_edges[0:len(bin_edges)-1] + delta_R/2
+    av_corr= np.divide(np.mean(corr,1),R*R)/(4*math.pi*delta_R*act_N*(act_N-1))*L**3
+    return av_corr, R
