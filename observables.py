1c32746053c3e7701610075f60123a92f9f6a877
adonvel <47566922+adonvel@users.noreply.github.com>
2019-03-22 14:19:18 +0100

    Update observables.py

--- observables.py
+++ observables.py
@@ -28,28 +28,6 @@ def p(press, nr_steps):
             np.random.choice(press, nr_steps))  # For  each bin, we take the mean of nr_steps randomly chosen values
     sigma_press = np.std(sample)
     return mean_press, sigma_press
-  av_corr= np.divide(np.mean(corr,1),R*R)/(4*math.pi*delta_R*act_N*(act_N-1))*L**3
-    sigma_corr=np.zeros(len(bin_edges)-1)
-    
-    #Bootstrapping error estimation
-    sample=np.zeros(50) #Resampling 500 times
-    for j in range(0,len(bin_edges)-1):#Sampling for each bin
-        for i in range(0,50): 
-            sample[i]=np.mean(np.random.choice(corr[j,:], nr_steps)) #For  each bin, we take the mean of nr_steps randomly chosen values
-        sigma_corr[j]=np.std(sample)
-    sigma_av_corr= np.divide(sigma_corr,R*R)/(4*math.pi*delta_R*act_N*(act_N-1))*L**3
-    return av_corr, R, sigma_corr
-
-def p(press, nr_steps):
-    
-    mean_press=np.mean(press)
-    
-    #Bootstrapping error estimation
-    sample=np.zeros(500)
-    for i in range(0,500): #Resampling 500 times
-            sample[i]=np.mean(np.random.choice(press, nr_steps))
-    sigma_press=np.std(sample)
-    return mean_press,  sigma_press
 
 def c(E_kin, nr_steps):
     #Bootstrapping calculation
@@ -62,5 +40,4 @@ def c(E_kin, nr_steps):
                                               
     av_specific_heat = np.mean(specific_heat)
     sigma_specific_heat = np.std(specific_heat)
-    
     return av_specific_heat, sigma_specific_heat
